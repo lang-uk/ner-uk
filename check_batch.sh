2886b5e53b7054aa565ac1198c7e858d7864a151
Vsevolod Dyomkin <vseloved@gmail.com>
2016-05-04 16:41:30 +0300

    Added check_batch script

---
+++ check_batch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+for filename in *.txt
+do
+    file=$(basename "$filename" .txt)
+    for dir1 in */
+    do
+        dir1=$(basename $dir1)
+        for dir2 in */
+        do
+            dir2=$(basename $dir2)
+            if [[ "$dir1" < "$dir2" ]] && [ -e $dir1/$file.ann ] && [ -e $dir2/$file.ann ]
+            then
+                echo $dir1 $dir2 $file.ann >> check_log.txt
+                diff --new-line-format='+%L' --old-line-format='-%L' --unchanged-line-format='%L' <(cut -f 2- $dir1/$file.ann) <(cut -f 2- $dir2/$file.ann) > $file.ann
+                
+                if [ $? -eq 0 ]
+                then
+                    awk 'BEGIN {i = 1} {if ($1 != "#") { print "T" i $0; i++}}' $file.ann > $file.ann.2
+                    mv $file.ann.2 $file.ann
+                elif [ $? -eq 1 ]
+                then
+                    mail -A $file.ann -s "Conflict in $file" vseloved@gmail.com < /dev/null
+                fi
+            fi
+         done
+    done
+done
