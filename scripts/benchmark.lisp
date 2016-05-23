25b472bcf36acf85828129c5bf5248671dc42eb5
Vsevolod Dyomkin <vseloved@gmail.com>
2016-05-23 22:10:46 +0300

    Improved benchmark script

--- scripts/benchmark.lisp
+++ scripts/benchmark.lisp
@@ -45,60 +45,67 @@
 (defun to-ent (str)
   (with (((_ tag beg end &rest ent) (split-if 'white-char-p str)))
     (pair (make-ent :beg (parse-integer beg)
-                      :end (parse-integer end)
-                      :wc (length ent)
+                    :end (parse-integer end)
+                    :wc (length ent)
                     :text (strjoin #\Space ent))
           (mkeyw tag))))
 
 (defun f1 (prec rec)
-  (float (/ (* 2 prec rec) (+ prec rec))))
-  
-(defun benchmark (gold-dir test-dir)
+  (let ((div (+ prec rec)))
+    (if (zerop div)
+        0
+        (float (/ (* 2 prec rec) div)))))
+
+(defun add-words (ent tag begs)
+  (let ((beg 0))
+    (dolist (word (split #\Space @ent.text))
+      (set# (+ @ent.beg beg) begs (pair ent tag))
+      (:+ beg (1+ (length word))))))
+
+(defun benchmark (gold-dir test-dir &key only-files verbose)
   (let ((total-gold 0.0)
         (total-test 0.0)
         (conf-mat #h(equal))
         tps tps2 tps3)
     (dolist (gold-file (directory (strcat gold-dir "*.ann")))
-      (with ((gold (pairs->ht (mapcar 'to-ent
-                                      (split #\Newline (read-file gold-file)
-                                             :remove-empty-subseqs t))
-                              :test 'equalp))
-             (test (pairs->ht (mapcar 'to-ent
-                                       (split #\Newline
-                                              (read-file
-                                               (fmt "~A/~A.ann" test-dir
-                                                    (pathname-name gold-file)))
-                                              :remove-empty-subseqs t))
-                              :test 'equalp))
-             (gold-begs #h())
-             (gold-ends #h())
-             (test-ends #h())
-             (test-begs #h()))
-        ;; match full
-        (dotable (ent tag gold)
-          (:+ (get# (pair tag :gold) conf-mat 0))
-          (if-it (? test ent)
-                 (if (eql tag it)
-                     (progn (push ent tps)
-                            (:+ (get# (pair tag tag) conf-mat 0)))
-                     (progn (push (pair ent it) tps2)
-                            (:+ (get# (pair tag it) conf-mat 0))))
-                 (:= (? gold-begs @ent.beg) (pair ent tag)
-                     (? gold-ends @ent.end) (pair ent tag))))
-        (dotable (ent tag test)
-          (:+ (get# (pair tag :test) conf-mat 0))
-          (set# @ent.beg test-begs (pair ent tag))
-          (set# @ent.end test-ends (pair ent tag)))
-        ;; match part
-        (dotable (beg ent-tag gold-begs)
-          (when-it (? test-begs beg)
-            (push (pair ent-tag it) tps3)
-            (rem# (? ent-tag 0 'end) gold-ends)))
-        (dotable (end ent-tag gold-ends)
-          (when-it (? test-ends end)
-            (push (pair ent-tag it) tps3)))
-        (:+ total-gold (ht-count gold))
-        (:+ total-test (ht-count test))))
+      (when verbose
+        (princ gold-file) (terpri))
+      (when (or (null only-files)
+                (member (pathname-name gold-file) only-files :test 'string=))
+        (with ((gold (pairs->ht (mapcar 'to-ent
+                                        (split #\Newline (read-file gold-file)
+                                               :remove-empty-subseqs t))
+                                :test 'equalp))
+               (test (pairs->ht (mapcar 'to-ent
+                                        (split #\Newline
+                                               (read-file
+                                                (fmt "~A/~A.ann" test-dir
+                                                     (pathname-name gold-file)))
+                                               :remove-empty-subseqs t))
+                                :test 'equalp))
+               (gold-begs #h())
+               (test-begs #h()))
+          ;; match full
+          (dotable (ent tag gold)
+            (:+ (get# (pair tag :gold) conf-mat 0))
+            (if-it (? test ent)
+                   (if (eql tag it)
+                       (progn
+                         (push it tps)
+                         (:+ (get# (pair tag tag) conf-mat 0)))
+                       (progn
+                         (push (pair ent it) tps2)
+                         (:+ (get# (pair tag it) conf-mat 0))))
+                   (add-words ent tag gold-begs)))
+          (dotable (ent tag test)
+            (:+ (get# (pair tag :test) conf-mat 0))
+            (add-words ent tag test-begs))
+          ;; match part
+          (dotable (beg ent-tag gold-begs)
+            (when-it (? test-begs beg)
+              (push (pair ent-tag it) tps3)))
+          (:+ total-gold (ht-count gold))
+          (:+ total-test (ht-count test)))))
     ;; calc stats
     (with ((tp (length tps))
            (tp2 (length tps2))
@@ -107,20 +114,27 @@
            (prec2 (/ (+ tp tp2) total-test))
            (rec2  (/ (+ tp tp2) total-gold))
            (tp3 0)
-           (tp4 0))
+           (tp4 0)
+           (clean-match-test total-test)
+           (clean-test total-test))
       ;; partial matches
       (dolist (tp tps3)
         (with ((((g-ent g-tag) (t-ent t-tag)) tp)
                (match% (/ (abs (- @g-ent.wc @t-ent.wc))
                           @g-ent.wc)))
           (if (eql g-tag t-tag)
-              (progn (:+ tp3 match%)
-                     (:+ (get# (pair g-tag g-tag) conf-mat 0) match%))
-              (progn (:+ tp4 match%)
-                     (:+ (get# (pair g-tag t-tag) conf-mat 0) match%)))))
-      (let ((prec3 (/ (+ tp tp3) total-test))
+              (progn
+                (:+ tp3 match%)
+                (:- clean-test)
+                (:- clean-match-test)
+                (:+ (get# (pair g-tag g-tag) conf-mat 0) match%))
+              (progn
+                (:+ tp4 match%)
+                (:- clean-test)
+                (:+ (get# (pair g-tag t-tag) conf-mat 0) match%)))))
+      (let ((prec3 (/ (+ tp) clean-match-test))
             (rec3  (/ (+ tp tp3) total-gold))
-            (prec4 (/ (+ tp tp2 tp3 tp4) total-test))
+            (prec4 (/ (+ tp tp2) clean-test))
             (rec4  (/ (+ tp tp2 tp3 tp4) total-gold)))
         (print-conf-mat conf-mat)
         (values #h(:match-full-all (float (/ tp total-gold))
@@ -129,15 +143,15 @@
                    :match-part-span (float (/ (+ tp tp2 tp3 tp4) total-gold))
                    :prec-full-all prec
                    :rec-full-all rec
+                   :f1-full-all (f1 prec rec)
                    :prec-full-span prec2
                    :rec-full-span rec2
+                   :f1-full-span (f1 prec2 rec2)
                    :prec-part-all prec3
                    :rec-part-all rec3
+                   :f1-part-all (f1 prec3 rec3)
                    :prec-part-span prec4
                    :rec-part-span rec4
-                   :f1-full (f1 prec rec)
-                   :f1-span (f1 prec2 rec2)
-                   :f1-part-full (f1 prec3 rec3)
                    :f1-part-span (f1 prec4 rec4))
                 conf-mat
                 (list tps tps2 tps3))))))
@@ -156,12 +170,12 @@
     (format t "~8<~A~>~C        ~{~8A~}~%" nil #\Tab
             (mapcar (lambda (tag)
                       (fmt-int-or-float
-                       (- (? conf-mat (pair tag :gold))
+                       (- (get# (pair tag :gold) conf-mat 0)
                            (reduce '+ (mapcar ^(get# (pair tag %) conf-mat 0)
                                               tags)))))
                     tags))
-    (dolist (test-tag (safe-sort tags '< :key ^(? conf-mat (pair % %))))
-      (let ((total-cnt (? conf-mat (pair test-tag :test)))
+    (dolist (test-tag (safe-sort tags '< :key ^(get# (pair % %) conf-mat 0)))
+      (let ((total-cnt (get# (pair test-tag :test) conf-mat 0))
             (matches-cnt (reduce '+ (mapcar ^(get# (pair % test-tag) conf-mat 0)
                                             tags))))
         (format t "~8<~A~>~C~8A~{~8A~}~%"
