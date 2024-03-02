cbf9980a3962771f9a37a447c7ce8cf609e4e234
Dmytro Chaplynskyi <chaplinsky.dmitry@gmail.com>
2024-03-03 01:27:59 +0200

    Cleaning up unused parts

--- scripts/align_after_seva.py
+++ scripts/align_after_seva.py
@@ -1,125 +1,11 @@
-from typing import List, Generator
+from typing import List
 import glob
 import shutil
 from pathlib import Path
 from collections import Counter
 import argparse
 
-from tqdm import tqdm
-
-from ner_utils import parse_bsf, BsfInfo, bsf_to_str, read_proofreaded_bsf_data
-
-
-def find_substring_occurrences(s: str, sub: str) -> Generator[int, None, None]:
-    """
-    Find all occurrences of the substring `sub` in the string `s`.
-    Args:
-        s: The string to search in
-        sub: The substring to search for
-    Returns:
-        A generator that yields the start position of each occurrence of `sub` in `s`
-
-    Example:
-    >>> list(find_substring_occurrences('this is a test text', 'test'))
-    [10]
-    >>> list(find_substring_occurrences('repeating repeating repeating', 'eating'))
-    [3, 13, 23]
-    """
-    start = 0  # Initial position to start searching from
-    while True:
-        start = s.find(sub, start)
-        if start == -1:  # If `sub` is not found, `find()` returns -1
-            break
-        yield start  # Yield the current start position
-        start += 1  # Move to the next position to continue searching
-
-
-def find_closest_occurrence(s: str, sub: str, start_idx: int) -> int:
-    """
-    Find the closest occurrence of the substring `sub` in the string `s` to the `start_idx`.
-    Args:
-        s: The string to search in
-        sub: The substring to search for
-        start_idx: The position to search around
-    Returns:
-        The start position of the closest occurrence of `sub` to `start_idx` in `s`
-        or -1 if not found
-    Example:
-    >>> find_closest_occurrence('here is a simple example example', 'example', 10)
-    17
-    >>> find_closest_occurrence('here is a simple example example', 'example', 27)
-    25
-    >>> find_closest_occurrence('nothing matches', 'example', 5)
-    -1
-    """
-
-    occurences = list(find_substring_occurrences(s, sub))
-
-    if not occurences:
-        return -1
-
-    return min(occurences, key=lambda x: abs(x - start_idx))
-
-
-def align_annotations(text_file: Path, annotation_file: Path) -> List[BsfInfo]:
-    """
-    Given the path to .txt and .ann files, align annotations with tokens.
-
-    Args:
-        text_file: Path to .txt file
-        annotation_file: Path to .ann file
-    Returns:
-        List of aligned BsfInfo objects
-    """
-
-    with text_file.open("r", encoding="utf-8") as f:
-        orig_txt: str = f.read()
-    with annotation_file.open("r", encoding="utf-8") as f:
-        ann: str = f.read()
-
-    text = orig_txt.replace("\n", " ")
-    offset = 0
-    result: List[BsfInfo] = []
-    prev_token: Optional[BsfInfo] = None
-    for bsf in parse_bsf(ann):
-        position = find_closest_occurrence(text, bsf.token, bsf.start_idx - offset)
-
-        if position == -1:
-            raise ValueError(f"Token not found: {bsf.token}")
-
-        new_start_idx = position + offset
-
-        if new_start_idx - bsf.start_idx != 0:
-            print(bsf.token, new_start_idx - bsf.start_idx)
-        # if abs(new_start_idx - bsf.start_idx) > 150:
-        #     print(
-        #         f"Token: {bsf.token} is not aligned with annotation: {bsf.start_idx} != {new_start_idx}"
-        #     )
-        # else:
-        #     print(
-        #         f"Token: {bsf.token} is aligned with annotation: {bsf.start_idx} => {new_start_idx}"
-        #     )
-
-        result.append(
-            BsfInfo(
-                id=bsf.id,
-                token=bsf.token,
-                start_idx=new_start_idx,
-                end_idx=new_start_idx + len(bsf.token),
-                tag=bsf.tag,
-            )
-        )
-
-        if prev_token and prev_token.start_idx <= bsf.start_idx < prev_token.end_idx:
-            # print(f"Overlapping tokens {prev_token.token} and {bsf.token}")
-            text = text[position:]
-            offset += position
-        else:
-            text = text[position + len(bsf.token) :]
-            offset += position + len(bsf.token)
-        prev_token = bsf
-
-    return result
+from ner_utils import BsfInfo, bsf_to_str, read_proofreaded_bsf_data
 
 
 def realign_annotations(text: str, bsfs: List[BsfInfo]) -> List[BsfInfo]:
@@ -247,7 +133,3 @@ if __name__ == "__main__":
     print(f"Tags count:")
     for tag, count in tags_count.most_common():
         print(f"\t{tag}: {count}")
-    # align_annotations(
-    #     text_file=Path("../data_2.0/0ac5140eb732.txt"),
-    #     annotation_file=Path("../data_2.0/0ac5140eb732.ann"),
-    # )
